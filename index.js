d4f257548084deedbc41a792189850018435923c
Meghla15 <meherunnesa1505@gmail.com>
2024-06-08 20:51:42 +0600

    token done

--- index.js
+++ index.js
@@ -38,12 +38,49 @@ async function run() {
       res.send({ token });
     })
 
+    // middlewares
+    const verifyToken = (req, res, next) =>{
+     
+      if (!req.headers.authorization){
+        return res.status(401).send({ message: 'unauthorized access' });
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+        if (err){
+          return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+      })
+
+    }
+
+      
+
     // user API
-    app.get('/users', async (req, res) => {
+    app.get('/users',verifyToken, async (req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
-      console.log(result)
+      // console.log(result)
     });
+
+    app.get('/users/admin/:email', verifyToken, async (req, res) => {
+      const email = req.params.email;
+
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ message: 'forbidden access' })
+      }
+
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if (user) {
+        admin = user?.role === 'admin';
+      }
+      res.send({ admin });
+    })
+
+    
     // user save to the database
     app.post('/users', async (req, res) => {
       const user = req.body;
